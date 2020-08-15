0f180aa6c94f2ef220f1f0c5caed51aefc158b0a
Matheus Pires <matheushenriquepires99@gmail.com>
2020-08-15 14:28:05 -0300

    Return user on session and CORS ajusts

--- server/app/Controllers/Http/SessionController.js
+++ server/app/Controllers/Http/SessionController.js
@@ -1,10 +1,17 @@
+const User = use('App/Models/User');
+
 class SessionController {
   async store({ request, auth }) {
     const { email, password } = request.only(['email', 'password']);
 
-    const token = await auth.attempt(email, password);
+    const user = await User.findBy('email', email);
 
-    return token;
+    const { token } = await auth.attempt(email, password);
+
+    return {
+      user,
+      token,
+    };
   }
 }
 
